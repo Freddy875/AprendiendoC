a88e8bdf24cdbcdf7d5363a5949b5d8ba979d147
Freddy875 <60365437+Freddy875@users.noreply.github.com>
2020-07-08 20:34:36 -0500

    Create Descendente.c
    Este programa utiliza un while para mostrar los primeros 10 números en pantalla en forma descendente

---
+++ While/Descendente.c
@@ -0,0 +1,19 @@
+// Mostrar los pirmeros 10 numeros en pantalla en forma descendente
+
+#include <stdio.h>
+int main(){
+	
+	int iContador = 10;
+	
+	while(iContador >= 1 ){
+		
+		printf("%d\n",iContador);
+				
+		iContador--;
+		
+	}//fin while 	
+	
+	return 0;
+	
+}//fin int main
+  
