890ee7ee7287ab8b02dd82452082f35ee73be05b
Freddy875 <fer875nandosm@gmail.com>
2020-07-16 13:56:10 -0500

    Creamos un programa para sumar los números impares

---
+++ For/SumaImpares.c
@@ -0,0 +1,39 @@
+//Suma todos los números impares
+#include <stdio.h>
+int main(){
+
+	int iContador, iParo, iSuma = 0 ;
+	
+	printf("Ingresa hasta que n%cmero quieres sumar: ",163);
+	
+	scanf("%d",&iParo);
+	
+	if(iParo <= 0 ){
+		
+		printf("ERROR. El n%cmero debe ser positivo y diferente de cero",163);
+		
+	}else{
+	
+		for(iContador = 0; iContador <= iParo; iContador++){
+		
+		if(iContador % 2 == 1){
+			
+			printf("\n%d + %d = %d",iSuma,iContador, iSuma = iSuma + iContador);		
+			
+		}else{
+			
+			//No hacer nada
+			
+		}//fin if-else
+		
+	}//fin for
+		
+	}//fin if-esle
+
+	printf("\nAl final la suma de todos los n%cmeros impares es: %d",163,iSuma);
+			
+	return 0;
+	
+}//fin int main
+
+
