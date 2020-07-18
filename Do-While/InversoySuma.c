914727e7405d8b3daf8d1e0a168edeb36a3a5376
Freddy875 <fer875nandosm@gmail.com>
2020-07-18 17:42:48 -0500

    Este programa recibe un numero entero y lo multiplica por menos uno lo hace varias veces hasta recibir un numero mayor de -10

---
+++ Do-While/InversoySuma.c
@@ -0,0 +1,49 @@
+/*
+Al ingresar un número entero desde teclado diferente de cero 
+Lo multiplica por menos uno 
+y lo hace varias veces hasta que se ingresa un número 
+mayor de -10 o menor de 10
+Y al final muestra la suma de todos los numeros ingresados  
+*/
+
+#include <stdio.h>
+int main(void){
+	
+	int iNumero,
+		iSuma = 0;
+		
+	do{
+		
+		printf("\nIngresa un n%cmero entero: ",163);
+		
+		scanf("%d",&iNumero);
+		
+		if(iNumero == 0){
+			
+			printf("\nERROR.El cero es ni positivo ni negativo");
+		}//fin if
+		
+		
+		iNumero *= (-1);
+		
+		printf("\nEl inverso de tu n%cmero es: %d",163,iNumero);
+		
+		//printf("\nEl inverso de tu n%cmero es: %d",163,-iNumero);
+		
+		//-iNumero;
+		
+		//printf("\nEl Inverso de tu n%cmero es: %d",163,iNumero);
+		
+		iSuma += iNumero;
+		
+		//iSuma = iSuma + iNumero;
+		
+	}while(iNumero >= -10 && iNumero <= 10);//fin do-while
+	
+	printf("\nEl resultado de la suma de todos los n%cmeros es: %d",163,iSuma);
+	
+	return 0;
+	
+}//fin int main
+
+
